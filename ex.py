59587a6c5a3258355e5c55b737a4d0510b7cc2d2
‘Yangwudy’ <3169260598@qq.com>
2022-05-22 22:28:16 +0800

    5.22.22

--- ex.py
+++ ex.py
@@ -1,164 +1,89 @@
-# -*- codeing = utf-8 -*-
-from bs4 import BeautifulSoup  # 网页解析，获取数据
-import re  # 正则表达式，进行文字匹配`
-import urllib.request, urllib.error  # 制定URL，获取网页数据
-import xlwt  # 进行excel操作
-#import sqlite3  # 进行SQLite数据库操作
+import requests
+import time
+from bs4 import BeautifulSoup
+import json
+# 首先我们写好抓取网页的函数
 
-findLink = re.compile(r'<a href="(.*?)">')  # 创建正则表达式对象，标售规则   影片详情链接的规则
-findImgSrc = re.compile(r'<img.*src="(.*?)"', re.S)
-findTitle = re.compile(r'<span class="title">(.*)</span>')
-findRating = re.compile(r'<span class="rating_num" property="v:average">(.*)</span>')
-findJudge = re.compile(r'<span>(\d*)人评价</span>')
-findInq = re.compile(r'<span class="inq">(.*)</span>')
-findBd = re.compile(r'<p class="">(.*?)</p>', re.S)
+#https://api.bilibili.com/x/v2/reply/main?&jsonp=jsonp&next=0&type=1&oid=552309529&mode=2&plat=1
+#Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.64 Safari/537.36 Edg/101.0.1210.53
+def get_html(url):
+    headers = {
+        'accept': '*/*',
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.64 Safari/537.36 Edg/101.0.1210.53',
+    }# 爬虫模拟访问信息
 
-
-
-
-def main():
-    baseurl = "https://movie.douban.com/top250?start="  #要爬取的网页链接
-    # 1.爬取网页
-    datalist = getData(baseurl)
-    savepath = "豆瓣电影Top250.xls"    #当前目录新建XLS，存储进去
-    # dbpath = "movie.db"              #当前目录新建数据库，存储进去
-    # 3.保存数据
-    saveData(datalist,savepath)      #2种存储方式可以只选择一种
-    # saveData2DB(datalist,dbpath)
+    r = requests.get(url, timeout=30,headers=headers)
+    r.raise_for_status()
+    r.endcodding = 'utf-8'
+    #print(r.text)
+    return r.text
 
 
-
-# 爬取网页
-def getData(baseurl):
-    datalist = []  #用来存储爬取的网页信息
-    for i in range(0, 10):  # 调用获取页面信息的函数，10次
-        url = baseurl + str(i * 25)
-        html = askURL(url)  # 保存获取到的网页源码
-        # 2.逐一解析数据
-        soup = BeautifulSoup(html, "html.parser")
-        for item in soup.find_all('div', class_="item"):  # 查找符合要求的字符串
-            data = []  # 保存一部电影所有信息
-            item = str(item)
-            link = re.findall(findLink, item)[0]  # 通过正则表达式查找
-            data.append(link)
-            imgSrc = re.findall(findImgSrc, item)[0]
-            data.append(imgSrc)
-            titles = re.findall(findTitle, item)
-            if (len(titles) == 2):
-                ctitle = titles[0]
-                data.append(ctitle)
-                otitle = titles[1].replace("/", "")  #消除转义字符
-                data.append(otitle)
-            else:
-                data.append(titles[0])
-                data.append(' ')
-            rating = re.findall(findRating, item)[0]
-            data.append(rating)
-            judgeNum = re.findall(findJudge, item)[0]
-            data.append(judgeNum)
-            inq = re.findall(findInq, item)
-            if len(inq) != 0:
-                inq = inq[0].replace("。", "")
-                data.append(inq)
-            else:
-                data.append(" ")
-            bd = re.findall(findBd, item)[0]
-            bd = re.sub('<br(\s+)?/>(\s+)?', "", bd)
-            bd = re.sub('/', "", bd)
-            data.append(bd.strip())
-            datalist.append(data)
-
-    return datalist
-
-
-# 得到指定一个URL的网页内容
-def askURL(url):
-    head = {  # 模拟浏览器头部信息，向豆瓣服务器发送消息
-        "User-Agent": "Mozilla / 5.0(Windows NT 10.0; Win64; x64) AppleWebKit / 537.36(KHTML, like Gecko) Chrome / 80.0.3987.122  Safari / 537.36"
-    }
-    # 用户代理，表示告诉豆瓣服务器，我们是什么类型的机器、浏览器（本质上是告诉浏览器，我们可以接收什么水平的文件内容
-
-    request = urllib.request.Request(url, headers=head)
-    html = ""
+def get_content(url):
+    '''
+    分析网页文件，整理信息，保存在列表变量中
+    '''
+    comments = []
+    # 首先，我们把需要爬取信息的网页下载到本地
+    html = get_html(url)
     try:
-        response = urllib.request.urlopen(request)
-        html = response.read().decode("utf-8")
-    except urllib.error.URLError as e:
-        if hasattr(e, "code"):
-            print(e.code)
-        if hasattr(e, "reason"):
-            print(e.reason)
-    return html
+        s=json.loads(html)
+    except:
+        print("jsonload error")
+    
+    num=len(s['data']['replies']) # 获取每页评论栏的数量
+    # print(num)
+    i=0
+    while i<num:
+        comment=s['data']['replies'][i]# 获取每栏评论
+
+        InfoDict={} # 存储每组信息字典
+        
+        InfoDict['Uname']=comment['member']['uname'] # 用户名
+        InfoDict['Like']=comment['like'] #点赞数
+        InfoDict['Content']=comment['content']['message'] # 评论内容
+        InfoDict['Time']=time.strftime("%Y-%m-%d %H:%M:%S",time.localtime(comment['ctime'])) # ctime格式的特殊处理？不太清楚具体原理
+        
+        comments.append(InfoDict)
+        i=i+1
+    # print(comments)
+    return comments
 
 
-# 保存数据到表格
-def saveData(datalist,savepath):
-    print("save.......")
-    book = xlwt.Workbook(encoding="utf-8",style_compression=0) #创建workbook对象
-    sheet = book.add_sheet('豆瓣电影Top250', cell_overwrite_ok=True) #创建工作表
-    col = ("电影详情链接","图片链接","影片中文名","影片外国名","评分","评价数","概况","相关信息")
-    for i in range(0,8):
-        sheet.write(0,i,col[i])  #列名
-    for i in range(0,250):
-        # print("第%d条" %(i+1))       #输出语句，用来测试
-        data = datalist[i]
-        for j in range(0,8):
-            sheet.write(i+1,j,data[j])  #数据
-    book.save(savepath) #保存
+def Out2File(dict):
+    '''
+    将爬取到的文件写入到本地
+    保存到当前目录的 BiliBiliComments.txt文件中。
+    '''
+    with open('BiliBiliComments.txt', 'a+',encoding='utf-8') as f:
+        i=0
+        for comment in dict:
+            i=i+1
+            try:
+                f.write('姓名：{}\t  点赞数：{}\t \n 评论内容：{}\t  评论时间：{}\t \n '.format(
+                    comment['Uname'], comment['Like'], comment['Content'], comment['Time']))
+                f.write("-----------------\n")
+            except:
+                print("out2File error")
+        print('当前页面保存完成')
 
-# def saveData2DB(datalist,dbpath):
-#     init_db(dbpath)
-#     conn = sqlite3.connect(dbpath)
-#     cur = conn.cursor()
-#     for data in datalist:
-#             for index in range(len(data)):
-#                 if index == 4 or index == 5:
-#                     continue
-#                 data[index] = '"'+data[index]+'"'
-#             sql = '''
-#                     insert into movie250(
-#                     info_link,pic_link,cname,ename,score,rated,instroduction,info)
-#                     values (%s)'''%",".join(data)
-#             # print(sql)     #输出查询语句，用来测试
-#             cur.execute(sql)
-#             conn.commit()
-#     cur.close
-#     conn.close()
-
-
-# def init_db(dbpath):
-#     sql = '''
-#         create table movie250(
-#         id integer  primary  key autoincrement,
-#         info_link text,
-#         pic_link text,
-#         cname varchar,
-#         ename varchar ,
-#         score numeric,
-#         rated numeric,
-#         instroduction text,
-#         info text
-#         )
-#
-#
-#     '''  #创建数据表
-#     conn = sqlite3.connect(dbpath)
-#     cursor = conn.cursor()
-#     cursor.execute(sql)
-#     conn.commit()
-#     conn.close()
-
-# 保存数据到数据库
-
-
-
-
-
-
-
-if __name__ == "__main__":  # 当程序执行时
-    # 调用函数
-     main()
-    # init_db("movietest.db")
-     print("爬取完毕！")
-
+if __name__ == '__main__':
+    e=0
+    page=1
+    while e == 0 and page<=100 :
+        #https://api.bilibili.com/x/v2/reply?pn=2&type=1&oid=455803935&sort=2
+        #请求 URL: https://api.bilibili.com/x/v2/reply/main?callback=jQuery1720992997518355516_1653227894164&jsonp=jsonp&next=0&type=1&oid=552309529&mode=3&plat=1&_=1653228539436
+        url = "https://api.bilibili.com/x/v2/reply/main?next={}&type=1&oid=552309529&mode=3".format(str(page))
+        try:
+            print()
+            
+            # print(url)
+            content=get_content(url)
+            print("page:",page)
+            Out2File(content)
+            page=page+1
+            # 为了降低被封ip的风险，每爬20页便歇5秒。
+            if page%10 == 0:
+                time.sleep(5)
+        except:
+            e=1
