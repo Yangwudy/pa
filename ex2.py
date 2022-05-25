271e1e4ff5f8333c484f9673002f53e9f57797ed
‘Yangwudy’ <3169260598@qq.com>
2022-05-25 15:52:28 +0800

    5.25.15

--- ex2.py
+++ ex2.py
@@ -1,14 +1,40 @@
 from aip import AipNlp
-import jieba
+import matplotlib.pyplot as plt
+ 
 APP_ID='26298747'
 API_KEY = 'DAbtxtwPotKfeHhDfPWq7yFw'
 SECRET_KEY = 'FmVMqjbUGcfMwq0bqS8jbNaQCdAQx4fd'
- 
-client = AipNlp(APP_ID, API_KEY, SECRET_KEY)
-txt = open("BiliBiliComments.txt", "r", encoding="utf-8").read()
-text=jieba.lcut(txt)
- 
-result=client.sentimentClassify(text)
 
-items=result['items']
-pos=items[0]['positive_prob']
+client = AipNlp(APP_ID, API_KEY, SECRET_KEY)
+txt = open("BC.txt", "r", encoding="utf-8").read()
+m=int(len(txt)/500)
+txt=list(txt)
+for i in range(1,m):
+    txt.insert(i*500,'#')
+txt=''.join(txt)
+x=txt.split('#')
+pos=0
+neg=0
+con=0
+m=0
+
+for i in range(59):
+    try:
+        result=client.sentimentClassify(x[i])
+    except:
+        if result.__contains__('items'):
+            items=result['items']
+            pos+=items[0]['positive_prob']
+            neg+=items[0]['negative_prob']
+            con=items[0]['confidence']
+            m+=1
+    else:
+        i+=1
+con=con/m
+
+plt.rcParams['font.sans-serif'] = 'simhei'
+data = [pos, neg]
+label = ['积极情绪', '消极情绪'] 
+plt.pie(data, labels = label, autopct='%.2f%%')
+plt.title('本分析置信度为{}'.format(con))
+plt.show()
--- ex3.py
+++ ex3.py
@@ -2,18 +2,21 @@ import requests
 import time
 from bs4 import BeautifulSoup
 import json
+
 def get_html(url):
     headers = {
         'accept': '*/*',
         'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.64 Safari/537.36 Edg/101.0.1210.53',
     }
+
     r = requests.get(url, timeout=30,headers=headers)
-    r.raise_for_status()  #如果网页状态码返回200，表示网页能正常访问，如果网页请求出错就不执行接下来的操作
+    r.raise_for_status()
     r.endcodding = 'utf-8'
     return r.text
 
 
 def get_content(url):
+    
     comments = []
     html = get_html(url)
     try:
@@ -21,31 +24,26 @@ def get_content(url):
     except:
         print("jsonload error")
     
-    num=len(s['data']['replies']) # 获取每页评论栏的数量
+    num=len(s['data']['replies'])
     i=0
     while i<num:
-        comment=s['data']['replies'][i]
-        InfoDict={} 
-        InfoDict['Uname']=comment['member']['uname'] 
-        InfoDict['Like']=comment['like'] 
-        InfoDict['Content']=comment['content']['message']
-        InfoDict['Time']=time.strftime("%Y-%m-%d %H:%M:%S",time.localtime(comment['ctime']))
+        comment=s['data']['replies'][i]# 获取每栏评论
+
+        InfoDict=comment['content']['message'] # 评论内容
         
         comments.append(InfoDict)
         i=i+1
     return comments
 
 
-def Out2File(dict):
-
-    with open('BiliBiliComments.txt', 'a+',encoding='utf-8') as f:
+def Out2File(con):
+    
+    with open('BC.txt', 'a+',encoding='utf-8') as f:
         i=0
-        for comment in dict:
+        for comment in con:
             i=i+1
             try:
-                f.write('姓名：{}\t  点赞数：{}\t \n 评论内容：{}\t  评论时间：{}\t \n '.format(
-                    comment['Uname'], comment['Like'], comment['Content'], comment['Time']))
-                f.write("-----------------\n")
+                f.write(comment)
             except:
                 print("out2File error")
         print('当前页面保存完成')
@@ -53,14 +51,15 @@ def Out2File(dict):
 if __name__ == '__main__':
     e=0
     page=1
-    while e == 0 and page<=100 :
+    while e == 0 and page<=20 :
         url = "https://api.bilibili.com/x/v2/reply/main?next={}&type=1&oid=552309529&mode=3".format(str(page))
         try:
+            print()
+            
+            # print(url)
             content=get_content(url)
             print("page:",page)
             Out2File(content)
             page=page+1
-            if page%10 == 0:
-                time.sleep(5)
         except:
             e=1
